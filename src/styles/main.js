59258a8a8957298e16b0da94be4a951b889c64b2
rriski <timo@totaltrivia.com>
2018-08-01 02:17:01 +0300

    Remove page transitions component and update styles (mainly font imports

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,46 +1,46 @@
 import { injectGlobal, css } from 'react-emotion';
 import {
   LibreBaskervilleRegular,
+  LatoLight,
   LatoRegular,
   LatoBlack,
-  LatoLight
+  MontserratBlack
 } from './fonts';
 
-const bgVars = {
-  bgColor: '#1370FB',
-  dotColor: '#3882E8',
-  dotSize: '2px',
-  dotSpace: '22px'
-};
-
 injectGlobal`
-  /* TODO check if importing fonts actually works like this */
   @font-face {
     font-family: "Libre Baskerville";
     font-style: normal;
     font-weight: normal;
-    src: local("Libre Baskerville"), local("Libre-baskerville"), url(${LibreBaskervilleRegular}) format("woff2");
-  }
-
-  @font-face {
-    font-family: "Lato Black";
-    font-style: normal;
-    font-weight: normal;
-    src: local("lato Black"), local("Lato-Black"), url(${LatoBlack}) format("woff2");
+    src: url(${LibreBaskervilleRegular}) format("woff2");
   }
 
   @font-face {
     font-family: "Lato Regular";
     font-style: normal;
     font-weight: normal;
-    src: local("Lato Regular"), local("Lato Regular"), url(${LatoRegular}) format("woff2");
+    src: url(${LatoRegular}) format("woff2");
   }
 
   @font-face {
     font-family: "Lato Light";
     font-style: normal;
     font-weight: normal;
-    src: local("Lato Light"), local("Lato Light"), url(${LatoLight}) format("woff2");
+    src: url(${LatoLight}) format("woff2");
+  }
+
+  @font-face {
+    font-family: "Lato Black";
+    font-style: normal;
+    font-weight: normal;
+    src: url(${LatoBlack}) format("woff2");
+  }
+
+  @font-face {
+    font-family: "Montserrat Black";
+    font-style: normal;
+    font-weight: normal;
+    src: url(${MontserratBlack}) format("woff2");
   }
 
   *, *:before, *:after {
@@ -51,9 +51,7 @@ injectGlobal`
     text-rendering: optimizeLegibility;
     overflow-x: hidden;
     box-sizing: border-box;
-    -ms-overflow-style: scrollbar;
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    background-color: #161719;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
@@ -63,14 +61,7 @@ injectGlobal`
     height: 100%;
     margin: 0;
     padding: 0;
-    font-family: 'Lato', sans-serif;
-
-    /* TODO */
-    background:
-		linear-gradient(90deg, ${bgVars.bgColor} (${bgVars.dotSpace} - ${bgVars.dotSize}), transparent 1%) center,
-		linear-gradient(${bgVars.bgColor} (${bgVars.dotSpace} - ${bgVars.dotSize}), transparent 1%) center,
-  		${bgVars.dotColor};
-  	background-size: ${bgVars.dotSpace} ${bgVars.dotSpace};
+    font-family: 'Libre Baskerville', sans-serif;
   }
 `;
 
