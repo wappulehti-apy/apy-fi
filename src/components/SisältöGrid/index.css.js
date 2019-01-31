197a3c4ca8177df2efb426e3a33e5062611a069d
rriski <timo.h.riski@outlook.com>
2019-01-31 23:38:58 +0200

    Fix sisältöpage margins

--- src/components/SisältöGrid/index.css.js
+++ src/components/SisältöGrid/index.css.js
@@ -9,8 +9,18 @@ export const GridContainer = styled.div`
   padding-top: 20px;
   margin: 0 25%;
 
-  ${media.desktop(css`
+  ${media.giant(css`
     margin: 0 25% auto 25%;
+    grid-template-columns: repeat(2, 1fr);
+  `)};
+
+  ${media.overdesktop(css`
+    margin: 0 20% auto 20%;
+    grid-template-columns: repeat(2 1fr);
+  `)};
+
+  ${media.desktop(css`
+    margin: 0 15% auto 15%;
     grid-template-columns: repeat(2, 1fr);
   `)};
 
@@ -22,6 +32,5 @@ export const GridContainer = styled.div`
   ${media.phone(css`
     margin: 0 2% auto 2%;
     grid-template-columns: repeat(1, 1fr);
-    font-size: 0.7em;
   `)};
 `;
