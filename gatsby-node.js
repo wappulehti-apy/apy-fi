5c5a9d0397ae82eaddd212fd824dad4da8255bb5
rriski <timo.h.riski@outlook.com>
2019-02-25 00:50:24 +0200

    Add redirect to handle äpy.fi domain in netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,21 @@ const path = require('path');
 // Initialize .env file
 require('dotenv').config();
 
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ actions, graphql }) => {
+  // need createRedirect action in actions collection
+  // to make the redirection magic happen.
+  // https://www.gatsbyjs.org/docs/bound-action-creators/
+  const { createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `https://www.xn--py-uia.fi/*`,
+    toPath: `https://www.apy.fi/:splat`,
+    isPermanent: true,
+  });
+};
+
 // Required for emotion (css in js lib) to work
 exports.onCreateBabelConfig = ({ actions: { setBabelPlugin } }) => {
   setBabelPlugin({ name: 'babel-plugin-emotion' });
