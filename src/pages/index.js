9c01fdac98583245c7734bbc10f9c29e9e7e055c
rriski <timo.h.riski@outlook.com>
2019-02-24 19:15:14 +0200

    Remove indexRef prop

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ class IndexPage extends React.PureComponent {
       process.env.GATSBY_THEME === 'ajaton' ? <LogoAjaton /> : <Logo2019 />;
     return (
       <Layout {...this.props}>
-        <ContainerIndex innerRef={this.indexRef}>
+        <ContainerIndex>
           {process.env.GATSBY_INDEX_VIDEO === 'true' ? <Video /> : Logo}
         </ContainerIndex>
       </Layout>
