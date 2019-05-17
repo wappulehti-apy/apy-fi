a4a83c5619579e932c15563320c3ac9cd4de4105
rriski <timo.h.riski@outlook.com>
2019-05-17 15:14:44 +0300

    Update scss variables

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,5 +1,22 @@
+// Layout
+export const marginGiant = '0 25%';
+export const marginOverdesktop = '0 20%';
+export const marginDesktop = '0 15%';
+export const marginTablet = '0 10%';
+export const marginPhone = '0 2%';
+
+export const paddingGiant = '1.5em 3em';
+export const paddingOverdesktop = '1.5em 3em';
+export const paddingDesktop = '1em 3em';
+export const paddingTablet = '1em 2em';
+export const paddingPhone = '1em';
+
+// Fonts
 export const headingFontBoldTheme = 'Montserrat Bold, sans-serif';
 export const headingFontBlackTheme = 'Montserrat Black, sans-serif';
 export const headingFontNormal = 'Libre Baskerville, sans-serif';
 export const textFont = 'Montserrat Black, sans-serif';
+
+// Styling
 export const highlightColor = '#9c223e';
+export const borderRadius = '2px';
