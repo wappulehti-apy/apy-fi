fb126f432778282a91aff72886dc38c2dc0adf1a
Timo Riski <timo.h.riski@aalto.fi>
2020-08-31 12:14:45 +0300

    Improve video styling

--- src/components/Video/index.css.js
+++ src/components/Video/index.css.js
@@ -1,9 +1,17 @@
 import styled from '@emotion/styled';
+import { media } from '../../styles/main';
 import { css } from '@emotion/core';
 
 export const PlayerContainer = styled.div`
   position: relative;
-  padding-top: 56.25%;
+  padding-top: 30.25%;
+  width: 60vw;
+  margin: 0 auto;
+
+  ${media.phone(css`
+    padding-top: 56.25%;
+    width: 95vw;
+  `)};
 `;
 
 export const playerStyle = css`
