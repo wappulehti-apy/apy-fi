27893bae273b81bd1f5679983bf3fcccc4378944
Timo Riski <25483483+rriski@users.noreply.github.com>
2020-12-16 01:41:02 +0200

    Add types to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
-      <Html>
+      <Html lang="fi">
         <Head />
         <body>
           <Main />
-          <div id="modal" />
           <NextScript />
+          <div id="modal" />
         </body>
       </Html>
     )
